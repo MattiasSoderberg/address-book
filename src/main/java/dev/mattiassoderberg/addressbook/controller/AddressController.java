1be3910b20501c07000c1e22ad37ec6b2fe64584
Mattias Söderberg <mattes.email@gmail.com>
2026-01-26 17:00:27 +0100

    Add get method findAll

--- src/main/java/dev/mattiassoderberg/addressbook/controller/AddressController.java
+++ src/main/java/dev/mattiassoderberg/addressbook/controller/AddressController.java
@@ -1,9 +1,25 @@
 package dev.mattiassoderberg.addressbook.controller;
 
+import dev.mattiassoderberg.addressbook.model.Address;
+import dev.mattiassoderberg.addressbook.repository.AddressRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/addresses")
 public class AddressController {
+
+    AddressRepository repository;
+
+    public AddressController(AddressRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<Address> findAll() {
+        return repository.findAll();
+    }
 }
