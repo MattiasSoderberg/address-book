57d02e9584294b40380567d11807ca2260b70e1c
Mattias Söderberg <mattes.email@gmail.com>
2026-01-26 16:20:22 +0100

    Add find methods

--- src/main/java/dev/mattiassoderberg/addressbook/repository/AddressRepository.java
+++ src/main/java/dev/mattiassoderberg/addressbook/repository/AddressRepository.java
@@ -1,4 +1,21 @@
 package dev.mattiassoderberg.addressbook.repository;
 
+import dev.mattiassoderberg.addressbook.model.Address;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
 public class AddressRepository {
+
+    List<Address> addresses = new ArrayList<>();
+
+    public List<Address> findAll() {
+        return addresses;
+    }
+
+    public Address findByName(String name) {
+        return addresses.stream().filter(address -> address.getName().equals(name)).findFirst().orElseThrow();
+    }
 }
