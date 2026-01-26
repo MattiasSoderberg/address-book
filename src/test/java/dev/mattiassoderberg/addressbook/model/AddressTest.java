f97349760a8e21e685e0fa354a7cdb6199c4ee13
Mattias Söderberg <mattes.email@gmail.com>
2026-01-26 16:05:51 +0100

    Add address test create

--- src/test/java/dev/mattiassoderberg/addressbook/model/AddressTest.java
+++ src/test/java/dev/mattiassoderberg/addressbook/model/AddressTest.java
@@ -1,7 +1,22 @@
 package dev.mattiassoderberg.addressbook.model;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AddressTest {
 
+    @Test
+    void createAddress() {
+        String name = "Test";
+        String street = "Test street";
+        String zipCode = "123 45";
+        String area = "Test area";
+        String city = "Testville";
+
+        Address address = new Address(name, street, zipCode, area, city);
+
+        assertNotNull(address);
+        assertEquals(street, address.getStreet());
+    }
 }
