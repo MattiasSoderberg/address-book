91f57971f2bc2d3c0fececc5d1c852d04d31645f
Mattias Söderberg <mattes.email@gmail.com>
2026-01-28 11:07:17 +0100

    Add exception package and AddressNotFoundExcteption

---
+++ src/main/java/dev/mattiassoderberg/addressbook/exception/AddressNotFoundException.java
@@ -0,0 +1,9 @@
+package dev.mattiassoderberg.addressbook.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class AddressNotFoundException extends Exception {
+
+}
