ebf5d51c20ca41be0ec7323af231848688fc2779
Mattias Söderberg <mattes.email@gmail.com>
2026-01-29 16:12:42 +0100

    Add validation annotations

--- src/main/java/dev/mattiassoderberg/addressbook/model/Address.java
+++ src/main/java/dev/mattiassoderberg/addressbook/model/Address.java
@@ -1,15 +1,30 @@
 package dev.mattiassoderberg.addressbook.model;
 
+import jakarta.validation.constraints.*;
+
 public class Address {
 
+    @NotBlank(message = "ID is required")
     private String id;
+
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @NotBlank(message = "Phone is required")
+    @Size(min = 10, max = 10, message = "Phone number must be 10 digits")
     private String phone;
+
+    @NotBlank(message = "Street is required")
     private String street;
+
+    @NotBlank(message = "Zip Code is required")
+    @Size(min = 5, max = 5, message = "Zip Code must be 5 digits")
     private String zipCode;
+
+    @NotBlank(message = "City is required")
     private String city;
 
-    public Address(String id, String name, String Phone, String street, String zipCode, String city) {
+    public Address(String id, String name, String phone, String street, String zipCode, String city) {
         this.id = id;
         this.name = name;
         this.phone = phone;
