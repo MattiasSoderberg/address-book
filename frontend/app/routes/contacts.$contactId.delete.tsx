e97b2cb3141ceb6e5aab42534b5e156212b18717
Mattias Söderberg <mattes.email@gmail.com>
2026-02-02 11:33:08 +0100

    Add delete contact route with only an action

---
+++ frontend/app/routes/contacts.$contactId.delete.tsx
@@ -0,0 +1,10 @@
+import { redirect } from "react-router";
+import type { Route } from "./+types/contacts.$contactId.delete";
+
+export async function action({ params }: Route.ActionArgs) {
+  await fetch(`${process.env.BASE_API_URL}/addresses/${params.contactId}`, {
+    method: "DELETE",
+  });
+
+  return redirect("/");
+}
