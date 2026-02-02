8277ff6322a3451decdd6a18ca276cc8043a95e5
Mattias Söderberg <mattes.email@gmail.com>
2026-02-02 11:31:07 +0100

    Add custom link component

---
+++ frontend/app/components/AppLink.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router";
+import { cn } from "~/utils";
+
+type Variant = "default" | "button";
+
+interface Props extends React.ComponentProps<"a"> {
+  to: string;
+  variant?: Variant;
+  state?: any;
+}
+
+const AppLink = ({
+  to,
+  children,
+  className,
+  variant = "default",
+  ...props
+}: Props) => {
+  const variantClasses = {
+    default: "hover:underline hover:opacity-70",
+    button:
+      "bg-blue-500 text-white px-4 py-2 rounded-xl inline-block hover:opacity-70",
+  };
+
+  return (
+    <Link to={to} className={cn(variantClasses[variant], className)} {...props}>
+      {children}
+    </Link>
+  );
+};
+
+export default AppLink;
