8b57e012fd5db504fd3b55ae76ba81c56bafbce8
Mattias Söderberg <mattes.email@gmail.com>
2026-02-04 14:18:36 +0100

    Set size for child component scroll

--- frontend/app/routes/contacts.add.tsx
+++ frontend/app/routes/contacts.add.tsx
@@ -37,7 +37,7 @@ export async function action({ request }: Route.ActionArgs) {
 
 export default function AddContact({ actionData }: Route.ComponentProps) {
   return (
-    <div className="flex flex-col gap-10 p-4">
+    <div className="size-full flex flex-col gap-10 p-4">
       <h2 className="text-2xl">Add Contact</h2>
       <FormComponent
         action="/contacts/new"
