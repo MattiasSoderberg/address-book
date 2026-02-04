4bc76477eaa83c439978c7e9ca0840f59b8ce1ed
Mattias Söderberg <mattes.email@gmail.com>
2026-02-04 15:11:29 +0100

    Add styles

--- frontend/app/components/contactImage.tsx
+++ frontend/app/components/contactImage.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const ContactImage = () => {
-  return <div>ContactImage</div>;
+type Props = {
+  imageUrl: string;
+};
+
+const ContactImage = ({ imageUrl }: Props) => {
+  return (
+    <div className="w-[200px] h-[200px] border border-gray-600 rounded-full overflow-hidden">
+      <img src={imageUrl} alt="" />
+    </div>
+  );
 };
 
 export default ContactImage;
