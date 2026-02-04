a7348d27ac3615a4cb1a7d95e55dff56474fc067
Mattias Söderberg <mattes.email@gmail.com>
2026-02-04 15:12:07 +0100

    Add input column wrapper

--- frontend/app/components/form.tsx
+++ frontend/app/components/form.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Form } from "react-router";
-import { TextInput, FileInput, type Errors } from "./input";
+import { TextInput, FileInput, type Errors, InputColumnWrapper } from "./input";
 import Button from "./button";
 import type { Contact } from "~/shared.types";
 
@@ -18,46 +18,52 @@ const FormComponent = ({
   errors,
 }: Props) => {
   return (
-    <Form className="" action={action} method="post">
-      <TextInput
-        label="Name"
-        type="text"
-        name="name"
-        defaultValue={contact?.name}
-        errors={errors}
-      />
-      <TextInput
-        label="Phone"
-        type="text"
-        name="phone"
-        defaultValue={contact?.phone}
-        errors={errors}
-      />
-      <TextInput
-        label="Street"
-        type="text"
-        name="street"
-        defaultValue={contact?.street}
-        errors={errors}
-      />
-      <TextInput
-        label="Zip Code"
-        type="text"
-        name="zipCode"
-        defaultValue={contact?.zipCode}
-        errors={errors}
-      />
-      <TextInput
-        label="City"
-        type="text"
-        name="city"
-        defaultValue={contact?.city}
-        errors={errors}
-      />
+    <Form
+      className="flex justify-between gap-4 overflow-y-auto"
+      action={action}
+      method="post"
+    >
+      <InputColumnWrapper>
+        <TextInput
+          label="Name"
+          type="text"
+          name="name"
+          defaultValue={contact?.name}
+          errors={errors}
+        />
+        <TextInput
+          label="Phone"
+          type="text"
+          name="phone"
+          defaultValue={contact?.phone}
+          errors={errors}
+        />
+        <TextInput
+          label="Street"
+          type="text"
+          name="street"
+          defaultValue={contact?.street}
+          errors={errors}
+        />
+        <TextInput
+          label="Zip Code"
+          type="text"
+          name="zipCode"
+          defaultValue={contact?.zipCode}
+          errors={errors}
+        />
+        <TextInput
+          label="City"
+          type="text"
+          name="city"
+          defaultValue={contact?.city}
+          errors={errors}
+        />
+        <Button type="submit" className="mt-6 px-8">
+          {buttonText}
+        </Button>
+      </InputColumnWrapper>
       <FileInput />
-      <Button type="submit" className="mt-6 px-8">
-        {buttonText}
-      </Button>
     </Form>
   );
 };
