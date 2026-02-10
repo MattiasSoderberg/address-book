5aa542782fb9a99345c721773f68a955fbe73a7d
Mattias Söderberg <mattes.email@gmail.com>
2026-02-10 09:56:31 +0100

    Refactor tests since the address class generates id

--- backend/src/test/java/dev/mattiassoderberg/addressbook/model/AddressTest.java
+++ backend/src/test/java/dev/mattiassoderberg/addressbook/model/AddressTest.java
@@ -2,22 +2,19 @@ package dev.mattiassoderberg.addressbook.model;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.UUID;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class AddressTest {
 
     @Test
     void createAddress() {
-        String id = UUID.randomUUID().toString();
         String name = "Test";
         String phone = "0701234567";
         String street = "Test street";
         String zipCode = "123 45";
         String city = "Testville";
 
-        Address address = new Address(id, name, phone, street, zipCode, city);
+        Address address = new Address(name, phone, street, zipCode, city);
 
         assertNotNull(address);
         assertEquals(street, address.getStreet());
