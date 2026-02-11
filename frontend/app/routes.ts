03a6b00df21ccc3513a0695678370696b2ce8cc5
Mattias Söderberg <mattes.email@gmail.com>
2026-02-11 14:41:01 +0100

    Change detail route to use id instead of name

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -3,7 +3,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
   index("routes/home.tsx"),
   route("contacts/new", "routes/contacts.add.tsx"),
-  route("contacts/:contactName", "routes/contacts.$contactName.tsx"),
+  route("contacts/:contactId", "routes/contacts.$contactId.tsx"),
   route("contacts/:contactId/edit", "routes/contacts.$contactId.edit.tsx"),
   route("contacts/:contactId/delete", "routes/contacts.$contactId.delete.tsx"),
 ] satisfies RouteConfig;
