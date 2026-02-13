caedcd3a3240d458faaef683d2d265b4d0d93bb4
Mattias Söderberg <mattes.email@gmail.com>
2026-02-13 07:26:01 +0100

    Refactor action to send image along with json

--- frontend/app/routes/contacts.add.tsx
+++ frontend/app/routes/contacts.add.tsx
@@ -1,38 +1,67 @@
 import { redirect } from "react-router";
 import type { Route } from "./+types/contacts.add";
 import FormComponent from "~/components/form";
+import { parseFormData, type FileUpload } from "@remix-run/form-data-parser";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Address Book" },
+    { name: "description", content: "New contact" },
   ];
 }
 
 export async function action({ request }: Route.ActionArgs) {
-  const formData = await request.formData();
-  const newContact = {
-    id: crypto.randomUUID(),
-    name: formData.get("name"),
-    phone: formData.get("phone"),
-    street: formData.get("street"),
-    zipCode: formData.get("zipCode"),
-    city: formData.get("city"),
+  const uploadHandler = (fileUpload: FileUpload) => {
+    return fileUpload.size > 0 ? fileUpload : null;
   };
 
-  const response = await fetch(`${process.env.BASE_API_URL}/addresses`, {
+  const formData = await parseFormData(request, uploadHandler);
+
+  const body = new FormData();
+  body.append(
+    "contact",
+    new Blob(
+      [
+        JSON.stringify(
+          {
+            name: formData.get("name"),
+            phone: formData.get("phone"),
+            street: formData.get("street"),
+            zipCode: formData.get("zipCode"),
+            city: formData.get("city"),
+          },
+          null,
+          2,
+        ),
+      ],
+      { type: "application/json" },
+    ),
+  );
+
+  if (formData.get("image")) {
+    body.append("image", formData.get("image") as Blob);
+    body.append("cropSize", formData.get("cropSize") as string);
+    body.append("cropX", formData.get("cropX") as string);
+    body.append("cropY", formData.get("cropY") as string);
+  }
+
+  const response = await fetch(`${process.env.BASE_API_URL}/contacts`, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json",
+      // "Content-Type": "multipart/form-data",
     },
-    body: JSON.stringify(newContact),
+    body,
   });
 
   if (!response.ok) {
     const errors = await response.json();
+    console.error("Error creating user ");
     return errors;
   }
-  return redirect(`/contacts/${newContact.name}`);
+
+  const data = await response.json();
+
+  return redirect(`/contacts/${data.id}`);
 }
 
 export default function AddContact({ actionData }: Route.ComponentProps) {
